891af1bd9f12e18a17b72d580c966261948cc006
Saintify13 <saintify200@yahoo.com>
2024-04-06 00:20:46 +0000

    Add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -34,9 +34,23 @@ function runGame(gameType) {
         throw`unknown game type: ${gameType}. Aborting!`;
     }
 }
-}
 
+/**
+ * Checks the answer against the first element in 
+ * the retured calcultedCorrectAnswer array
+ */
 function checkAnswer() {
+
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculedAnswer[0];
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awwww....You answered ${userAnswer}. The correct answer was ${calculedAnswer[0]}!`);
+    }
+
+    runGame(calculedAnswer[1]);
 
 }
 
